fffac6376263e2221b43def07e11dc1c236369b1
rajan52 <34733613+rajan52@users.noreply.github.com>
2021-05-30 20:15:14 +0530

    discount

---
+++ Server_DotNetCore/ShoppingCart.Discount/IDiscount.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCart.Discount
+{
+    public interface IDiscount
+    {
+        decimal apply(decimal originalPrice);
+    }
+}
