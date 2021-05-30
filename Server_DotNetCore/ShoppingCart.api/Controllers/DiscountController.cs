a3f67adca20c48914c9412f25dc51d21ea3dcfc7
rajan52 <34733613+rajan52@users.noreply.github.com>
2021-05-30 19:54:34 +0530

    added api

---
+++ Server_DotNetCore/ShoppingCart.api/Controllers/DiscountController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingCart.Model.Dto;
+using ShoppingCart.Service;
+
+namespace ShoppingCart.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("AllowMyOrigin")]
+    [Authorize]
+    public class DiscountController : ControllerBase
+    {
+
+        ICustomerService customerService;
+        public DiscountController(ICustomerService _customerService)
+        {
+            this.customerService = _customerService;
+        }
+        public ActionResult Post(CustomerDto customerDto)
+        {
+            try
+            {
+                var custdto = this.customerService.Discount(customerDto);
+                if (custdto != null)
+                {
+                    return Ok(custdto);
+                }
+                else
+                {
+                    return NotFound("invalid customer");
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+    }
+}
