7ea63815e3c75b5bb6dbb9bf0d5c422460ec1ec1
rajan52 <34733613+rajan52@users.noreply.github.com>
2021-05-30 20:16:51 +0530

    Factory

---
+++ Server_DotNetCore/ShoppingCart.Model.Factory/CustomerFactory.cs
@@ -0,0 +1,38 @@
+﻿using ShoppingCart.Discount;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCart.Model.Factory
+{
+   public class CustomerFactory : ICustomerFactory
+    {
+        private Dictionary<string, ICustomer> customerList=null;
+        public CustomerFactory()
+        {
+
+            customerList = new Dictionary<string, ICustomer>();
+
+            NormaluserDiscount normaluserDiscount = new NormaluserDiscount();
+            PrivilegedUserDiscount privilegedUserDiscount = new PrivilegedUserDiscount(normaluserDiscount);
+
+            ICustomer normalCustomer = new NormalCustomer(normaluserDiscount);
+            ICustomer priviagedCustomer = new PriviagedCustomer(privilegedUserDiscount);
+
+            customerList.Add("normal", normalCustomer);
+            customerList.Add("privileged", priviagedCustomer);
+        }
+        /// <summary>
+        /// idealy this should be injected via depenedency injection.
+        /// but.net core does not have support to inject depenedency in decortor pattern style
+        /// hence using factory pattern
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        //
+        public ICustomer GetCustomer(string type)
+        {
+           return customerList.ContainsKey(type)? customerList[type] : throw new Exception("Invalid Customer Type");
+        }
+    }
+}
---
+++ Server_DotNetCore/ShoppingCart.Model.Factory/ICustomerFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCart.Model.Factory
+{
+    public interface ICustomerFactory
+    {
+        ICustomer GetCustomer(string type);
+    }
+}
