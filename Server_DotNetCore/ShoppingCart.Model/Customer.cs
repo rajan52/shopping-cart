6d8c69643d6adb4cbcec8add39ba2e30dd460055
rajan52 <34733613+rajan52@users.noreply.github.com>
2021-05-30 20:15:54 +0530

    model

---
+++ Server_DotNetCore/ShoppingCart.Model/Customer.cs
@@ -0,0 +1,23 @@
+﻿using ShoppingCart.Discount;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCart.Model
+{
+    public abstract class Customer : ICustomer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public decimal TotalPrce { get; set; }
+        public abstract decimal DiscountedPrice { get; }
+
+        protected IDiscount discount;
+        public Customer(IDiscount _discount)
+        {
+            this.discount = _discount;
+        }
+    }
+}
---
+++ Server_DotNetCore/ShoppingCart.Model/Dto/TokenDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCart.Model.Dto
+{
+   public class TokenDto
+    {
+        public string Token { get; set; }
+    }
+}
