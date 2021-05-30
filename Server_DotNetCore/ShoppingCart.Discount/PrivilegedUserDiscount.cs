28d93004960568e5c568afe87ae0fecfc640344c
rajan52 <34733613+rajan52@users.noreply.github.com>
2021-05-30 20:23:24 +0530

    Add files via upload

---
+++ Server_DotNetCore/ShoppingCart.Discount/PrivilegedUserDiscount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCart.Discount
+{
+    public class PrivilegedUserDiscount : DiscountDecorator
+    {
+        const decimal DISCOUNT_PERCENTAGE = 2;// this value can be read from config also
+        public PrivilegedUserDiscount(IDiscount _discount) : base(_discount)
+        {
+        }
+        public override decimal apply(decimal originalPrice)
+        {
+            decimal discountpricce = base.apply(originalPrice);
+            return discountpricce - ((discountpricce * DISCOUNT_PERCENTAGE) / 100);
+        }
+    }
+}
