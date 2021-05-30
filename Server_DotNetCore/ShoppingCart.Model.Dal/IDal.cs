8b73bc8804436f5b5e7d20c751607b0942256d77
rajan52 <34733613+rajan52@users.noreply.github.com>
2021-05-30 19:57:46 +0530

    added dal factory

---
+++ Server_DotNetCore/ShoppingCart.Model.Dal/IDal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCart.Model.Dal
+{
+    public interface IDal
+    {
+        ICustomer AddCustomer(ICustomer cust);
+        ICustomer GetCustomer(string userName,string password);
+        ICustomer GetCustomer(int id);
+    }
+}
