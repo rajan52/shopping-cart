ec445683617f7d648b3f4e1e577cf946c22257b7
rajan52 <34733613+rajan52@users.noreply.github.com>
2021-05-30 19:56:22 +0530

    added discount

---
+++ Server_DotNetCore/ShoppingCart.Discount/DiscountDecorator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCart.Discount
+{
+    public abstract class DiscountDecorator : IDiscount
+    {
+        protected IDiscount discount;
+        public DiscountDecorator(IDiscount _discount)
+        {
+            this.discount = _discount;
+        }
+
+        public virtual decimal apply(decimal originalPrice)
+        {
+            return discount.apply(originalPrice);
+        }
+    }
+}
