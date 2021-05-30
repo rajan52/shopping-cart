6c71ab3b812a49357da737e3ad1e82687a16a170
rajan52 <34733613+rajan52@users.noreply.github.com>
2021-05-30 20:26:54 +0530

    model

---
+++ Server_DotNetCore/ShoppingCart.Model/PriviagedCustomer.cs
@@ -0,0 +1,22 @@
+﻿using ShoppingCart.Discount;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCart.Model
+{
+    public class PriviagedCustomer : Customer
+    {
+        public PriviagedCustomer(IDiscount _discount) : base(_discount)
+        {
+
+        }
+        public override decimal DiscountedPrice
+        {
+            get
+            {
+                return this.discount.apply(this.TotalPrce);
+            }
+        }
+    }
+}
