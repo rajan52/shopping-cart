b94485cc568ea1e639c42b737858edaa577404fc
rajan52 <34733613+rajan52@users.noreply.github.com>
2021-05-30 20:12:03 +0530

    Add files via upload

---
+++ Server_DotNetCore/ShoppingCart.Model/ICustomer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCart.Model
+{
+    public interface ICustomer
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        string UserName { get; set; }
+        string Password { get; set; }
+        decimal TotalPrce { get; set; }
+        decimal DiscountedPrice { get; }
+    }
+}
---
+++ Server_DotNetCore/ShoppingCart.Model/NormalCustomer.cs
@@ -0,0 +1,23 @@
+﻿using ShoppingCart.Discount;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCart.Model
+{
+    public class NormalCustomer : Customer
+    {
+        public NormalCustomer(IDiscount _discount) : base(_discount)
+        {
+
+        }
+
+        public override decimal DiscountedPrice
+        {
+            get
+            {
+                return this.discount.apply(this.TotalPrce);
+            }
+        }
+    }
+}
